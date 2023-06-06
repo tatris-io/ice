7439e24ff291c5d1685651ea9e6af48cab325b5d
sanhuan.sw <sanhuan.sw@alibaba-inc.com>
2023-06-06 17:34:02 +0800

    fix: fix init docVisitState

--- docvalues.go
+++ docvalues.go
@@ -282,7 +282,7 @@ func (s *Segment) visitDocumentFieldTerms(localDocNum uint64, fields []string,
 	visitor segment.DocumentValueVisitor, dvs *docVisitState) (
 	*docVisitState, error) {
 	if dvs == nil {
-		dvs = &docVisitState{}
+		dvs = &docVisitState{segment: s}
 	} else if dvs.segment != s {
 		dvs.segment = s
 		dvs.dvrs = nil
